905ec151fee14384c73be76253caf458b043d4f0
tkaracam <tolgahankaracam@outlook.de>
2026-03-29 00:01:25 +0100

    Fix CORS headers for API endpoints

--- api/questions.php
+++ api/questions.php
@@ -5,6 +5,13 @@
 
 header('Content-Type: application/json; charset=utf-8');
 header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type');
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit;
+}
 
 $module = $_GET['module'] ?? '';
 $teil = $_GET['teil'] ?? '';
